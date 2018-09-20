4c8eb9bd13f9a3968cf876dd9adfaaed816ed915
William Fabre <contact@williamfabre.fr>
2018-09-20 15:45:51 +0200

    some problem with destructors

--- ctme2/src/Box.cpp
+++ ctme2/src/Box.cpp
@@ -1,42 +1,83 @@
 #include <iostream>
+#include "Box.h"
 using namespace std;
 
-void Box::Box()
+void swap(long* a, long* b)
 {
+	long tmp = *a; *a = *b; *b = tmp;
+}
+
+long min(long a, long b)
+{
+	return (a > b) ? b : a;
+}
+
+long max(long a, long b)
+{
+	return (a > b) ? a : b;
+}
+
+Box::Box()
+{
+	name_ = "Unknow";
+	x1_ = 1;
+	y1_ = 1;
+	x2_ = 1;
+	y2_ = 1;
+}
+
+Box::Box(string name, long x1, long y1, long x2, long y2)
+{
+	name_ = name;
+	x1_ = x1;
+	y1_ = y1;
+	x2_ = x2;
+	y2_ = y2;
+}
+
+Box::Box(const Box& other)
+{
+	name_ = other.name_;
+	x1_ = other.x1_;
+	y1_ = other.y1_;
+	x2_ = other.x2_;
+	y2_ = other.y2_;
+}
+
+Box::~Box(){
 	return;
 }
 
-void Box::Box(string name, long x1, long y1, long x2, long y2)
+inline bool Box::isEmpty() const
+{ return (x1_ > x2_) || (y1_ > y2_); }
+
+
+string Box::getName() const
 {
-	return;
+	return name_;
 }
 
-void Box::Box(const Box& box)
+long Box::getX1() const
 {
-	return;
+	return x1_;
 }
 
-inline bool Box::isEmpty()
-{
-	return 0;
-}
-
-string Box::getName()
-{
-	return "";
-}
-
-long Box::getX1()
+bool Box::intersect(const Box&) const
 {
 	return 0;
 }
 
 Box& Box::inflate(long dxy)
 {
-	return NULL;
+	x1_ -= dxy;
+	y1_ -= dxy;
+	x2_ += dxy;
+	y2_ += dxy;
+	//PROBLEM
 }
 
-Box Box::getIntersection(const Box& box)
+Box Box::getIntersection(const Box& box) const
 {
-	return NULL
+	//PROBLEM
+	return Box();
 }
--- ctme2/src/Main.cpp
+++ ctme2/src/Main.cpp
@@ -20,4 +20,21 @@ Box testFunction(Box ab)
 	return ab;
 }
 
+int main (int argc, char* argv[])
+{
+	{
+		Box b0;
+		Box b1 ("b1", 15, 10, 30, 20);
+		// cerr << "allocated Boxes:" << Box::getAllocateds() << endl;
+		cerr << "\nTest 1:  "; b1.print(cerr); cerr << endl;
+		testFunction(b1);
 
+		b1.inflate(5,5,5,5);
+		cerr << "\nTest 2:  "; b1.print(cerr); cerr << endl;
+		testFunction(b1);
+
+		cerr << "\nTest 3" << endl;
+		cerr << "+ Box \"" << endl;
+	}
+	return 0;
+}
